3dea9642f2a9e41ee74a4e977a230238b8ed19ff
Aci <hoangn1626@gmail.com>
2026-01-12 01:12:16 +0700

    Enhance quickbuttons with config and toggle support
    Added configuration management and toggle button functionality.

--- modules/quickbuttons.lua
+++ modules/quickbuttons.lua
@@ -10,6 +10,12 @@ end
 local main, screenui = {}, Instance.new("ScreenGui", core)
 screenui.Name = "HHxScripts_SGUI"
 
+local configs = {
+  saving_state = false,
+  set = false,
+  buttons = {}
+}
+
 function adduicorner(t, r) Instance.new("UICorner", t).CornerRadius = UDim.new(r, 0) end
 function do_notify(str) sterui:SetCore("SendNotification", {Title = "MODULE NOTIFY", Text = str, Duration = 1.25,}) end
 
@@ -19,7 +25,14 @@ function randomstrs() local keys, newest = "abcdefghy", ""
   end return newest
 end
 
+main.add_config = function(cfgs)
+  if type(cfgs) == "table" then
+    for key, val in pairs(cfgs) do if configs[key] ~= nil then configs[key] = val end end
+  end configs.set = true
+end
+
 main.add_button = function(keys, fname, tcolor, pos, crner, script)
+  if not configs.set then do_notify("must use function 'add_config({ key = val })' first, before run this function.") return end
   local new_btn = Instance.new("TextButton", screenui)
   new_btn.Name = randomstrs()
   new_btn.BackgroundTransparency = 0.5
@@ -37,7 +50,10 @@ main.add_button = function(keys, fname, tcolor, pos, crner, script)
   return new_btn
 end
 
-main.add_toggle = function(keys, fname, tcolor, pos, crner, script)
+main.add_toggle = function(keys, fname, tcolor, pos, crner, script, var)
+  if not configs.set then do_notify("must use function 'add_config({ key = val })' first, before run this function.") return end
+  local varx = var or "none"
+  if varx ~= "none" and type(varx) == "string" then configs.buttons[varx] = false end
   local new_btn = Instance.new("TextButton", screenui)
   new_btn.Name = randomstrs()
   new_btn.BackgroundTransparency = 0.5
@@ -51,12 +67,24 @@ main.add_toggle = function(keys, fname, tcolor, pos, crner, script)
   new_btn.Font = Enum.Font[fname]
   new_btn.Visible = true
   adduicorner(new_btn, crner)
-  local var = false
+  local btn_state = false
   new_btn.MouseButton1Click:Connect(function()
-    if not var then var = true else var = false end
-    while var do wait(0.01) script() end
-  end)
-  return new_btn
+    if varx ~= "none" then
+      if not configs.buttons[varx] then configs.buttons[varx] = true
+        new_btn.TextColor3 = Color3.fromRGB(0, 255, 0)
+      else configs.buttons[varx] = false
+        new_btn.TextColor3 = Color3.fromRGB(unpack(tcolor))
+      end
+    else
+      if not btn_state then btn_state = true
+        new_btn.TextColor3 = Color3.fromRGB(0, 255, 0)
+      else btn_state = false
+        new_btn.TextColor3 = Color3.fromRGB(unpack(tcolor))
+      end
+    end
+  end) while btn_state or configs.buttons[varx] do wait(0.01)
+    script()
+  end return new_btn
 end
 
 main.removing_buttons = function()
