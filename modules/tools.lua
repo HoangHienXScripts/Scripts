9381c88c4932a4dd9e73f45e1a0249be5b6c61f9
Aci <hoangn1626@gmail.com>
2026-01-12 13:03:00 +0700

    Refactor tools module for player interaction and tweening

--- modules/tools.lua
+++ modules/tools.lua
@@ -1,13 +1,25 @@
 -- Tools module by HoangHien v0.1 --
 local plrs = game:GetService("Players")
+local tws = game:GetService("TweenService")
 local plr = plrs.LocalPlayer
+
+local data = {
+    ctws = nil
+}
 
 local modules = {}
 modules.__index = modules
 
 local fun = function(name) return game:GetService(name) end
 local is_alive = function(usr)
-  return usr and usr.Character and usr.Character:FindFirstChildOfClass("Humanoid") and usr.Character.Humanoid.Health > 0
+    return usr and usr.Character and usr.Character:FindFirstChildOfClass("Humanoid") and usr.Character.Humanoid.Health > 0
+end
+local create_tween = function(t1, t2, up_axis)
+    if data.ctws ~= nil then data.ctws:Cancel() end
+	up_axis = up_axis or 0
+	data.ctws = tws:Create(t1, TweenInfo.new((t1.Position - t2.Position).magnitude / 195, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut), {
+			CFrame = CFrame.new(t2.Position + Vector3.new(0, up_axis, 0)
+	}) data.ctws:Play()
 end
 
 function modules:new(sv)
@@ -23,7 +35,7 @@ function modules:get_nearest()
 	for _, usr in pairs(self.SERVICE:GetPlayers()) do
 		if usr ~= plr and is_alive(usr) and usr.Character:FindFirstChild("HumanoidRootPart") then
 			local direct = (usr.Character.HumanoidRootPart.Position - plr.Character.HumanoidRootPart.Position).magnitude
-			if direct < data.mag and direct <= data.inr then
+			if direct < data.mag and direct <= data.inr and is_alive(usr) then
 				data.mag = direct
 				data.near = usr
 			end
@@ -32,9 +44,28 @@ function modules:get_nearest()
 	return self
 end
 
-function modules:print_name()
+function modules:teleport(up_axis)
+    if self.NEARBY then
+        local hrp = self.NEARBY.Character:FindFirstChild("HumanoidRootPart")
+		up_axis = up_axis or 0
+		if hrp and plr and plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") then
+            plr.Character.HumanoidRootPart.CFrame = CFrame.new(hrp.Position + Vector3.new(0, up_axis, 0))
+		end
+	end return self
+end
+
+function modules:tweening_to(up_axis)
+    if self.NEARBY then
+        local hrp = self.NEARBY.Character:FindFirstChild("HumanoidRootPart")
+		if hrp and plr and plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") then
+            create_tween(plr.Character.HumanoidRootPart, hrp, up_axis)
+		end
+	end return self
+end
+
+function modules:get_username()
 	if self.NEARBY then print("Name: " .. self.NEARBY.Name) end
-	return self
+	return self.NEARBY.Name
 end
 
 return modules
