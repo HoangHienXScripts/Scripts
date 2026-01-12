5a9dbee2dc8471a5c9bd33870964663a8916dfbe
Aci <hoangn1626@gmail.com>
2026-01-12 12:16:10 +0700

    Refactor quick buttons module for better structure
    Refactor and optimize the quick buttons module, improving function definitions and organization.

--- modules/quickbuttons.lua
+++ modules/quickbuttons.lua
@@ -1,123 +1,116 @@
--- buttons --
+-- HoangHien UI-Library (First Time: v0.2) --
+local modules = {}
+
 local core, starterui, htp
 core = game:GetService("CoreGui")
-sterui = game:GetService("StarterGui")
+starterui = game:GetService("StarterGui")
 htp = game:GetService("HttpService")
+pcall(function() if core:FindFirstChild("HHxScripts_SGUI") then core.HHxScripts_SGUI:Destroy() end end)
 
-if core:FindFirstChild("HHxScripts_SGUI") then core.HHxScripts_SGUI:Destroy() 
-  print("Previous 'ScreenGui' Loaded, Has been removed.")
-end
+local inst_s, configs = {
+    screenui = Instance.new("ScreenGui")
+}, {
+    ready = false, save = false, buttons = {}
+} inst_s.screenui.Parent = core
+inst_s.screenui.Name = "HHxScripts_SGUI"
 
-local main, screenui = {}, Instance.new("ScreenGui", core)
-screenui.Name = "HHxScripts_SGUI"
-local folders = {"HHxScripts", "HHxScripts/Games", "HHxScripts/Games/PLACE_" .. tostring(game.GameId) .. "_SAVEFOLDER"}
-local configs = {
-  saving_state = false,
-  set = false,
-  buttons = {}
+local folders = {
+    "HHxScripts", "HHxScripts/Games", "HHxScripts/Games/PLACE_" .. game.GameId .. "_SAVEFOLDER"
 }
 
-function adduicorner(t, r) Instance.new("UICorner", t).CornerRadius = UDim.new(r, 0) end
-function do_notify(str) sterui:SetCore("SendNotification", {Title = "MODULE NOTIFY", Text = str, Duration = 1.25,}) end
-
-function randomstrs() local keys, newest = "abcdefghy", ""
-  for i = 1, #keys do
-    newest = newest .. keys:sub(math.random(1, #keys), math.random(1, #keys))
-  end return newest
-end
-
-function createsave_folder()
-  if isfolder and makefolder and isfile and writefile and readfile then
-    for index = 1, #folders do if not isfolder(folders[index]) then makefolder(folders[index]) end
-    if not isfile(folders[3] .. "/QuickButtons.json") then
-      writefile(folders[3] .. "/QuickButtons.json", htp:JSONEncode(configs.buttons))
+function randomstr(leng)
+    local chars = "abcdefghijklmnopqrstuvwxyz"
+    local s = ""
+    for i = 1, (leng or 10) do
+        local r = math.random(1, #chars)
+        s = s .. chars:sub(r, r)
     end
-  end
-end
+    return s
+end function round_ui(obj, scale) Instance.new("UICorner", obj).CornerRadius = UDim.new(scale or 0, 0) end
+function notify(t) starterui:SetCore("SendNotification", {Title = "《XScripts - UIModule》", Text = t, Duration = 1.25}) end
 
-local ppx = 0
-function update_configs()
-  if not configs.set and isfolder(folders[3]) then
-    configs.saving_state = true
-    configs.set = true
-  end if isfile(folders[3] .. "/QuickButtons.json") then
-    configs.buttons = htp:JSONDecode(readfile(folders[3] .. "/QuickButtons.json"))
-    if ppx > 0 then
-      writefile(folders[3] .. "/QuickButtons.json", htp:JSONEncode(configs.buttons))
-    end ppx += 1
-  end
-end update_configs()
-
-main.add_config = function(cfgs)
-  if type(cfgs) == "table" then
-    for key, val in pairs(cfgs) do if configs[key] ~= nil then configs[key] = val end end
-  end configs.set = true
-  if configs.saving_state then createsave_folder() end
-end
-
-main.add_button = function(keys, fname, tcolor, pos, crner, script)
-  if not configs.set then do_notify("must use function 'add_config({ key = val })' first, before run this function.") return end
-  local new_btn = Instance.new("TextButton", screenui)
-  new_btn.Name = randomstrs()
-  new_btn.BackgroundTransparency = 0.5
-  new_btn.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-  new_btn.Position = UDim2.new(unpack(pos))
-  new_btn.Size = UDim2.new(0.05, 0, 0.1, 0)
-  new_btn.TextScaled = true
-  new_btn.TextSize = 12
-  new_btn.TextColor3 = Color3.fromRGB(unpack(tcolor))
-  new_btn.Text = keys
-  new_btn.Font = Enum.Font[fname]
-  new_btn.Visible = true
-  adduicorner(new_btn, crner)
-  new_btn.MouseButton1Click:Connect(script)
-  return new_btn
-end
-
-main.add_toggle = function(keys, fname, tcolor, pos, crner, script, var)
-  if not configs.set then do_notify("must use function 'add_config({ key = val })' first, before run this function.") return end
-  local varx = var or "none"
-  if varx ~= "none" and type(varx) == "string" then configs.buttons[varx] = false end
-  local new_btn = Instance.new("TextButton", screenui)
-  new_btn.Name = randomstrs()
-  new_btn.BackgroundTransparency = 0.5
-  new_btn.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-  new_btn.Position = UDim2.new(unpack(pos))
-  new_btn.Size = UDim2.new(0.05, 0, 0.1, 0)
-  new_btn.TextScaled = true
-  new_btn.TextSize = 12
-  new_btn.TextColor3 = Color3.fromRGB(unpack(tcolor))
-  new_btn.Text = keys
-  new_btn.Font = Enum.Font[fname]
-  new_btn.Visible = true
-  adduicorner(new_btn, crner)
-  local btn_state = false
-  new_btn.MouseButton1Click:Connect(function()
-    if varx ~= "none" then
-      if not configs.buttons[varx] then configs.buttons[varx] = true
-        new_btn.TextColor3 = Color3.fromRGB(0, 255, 0)
-      else configs.buttons[varx] = false
-        new_btn.TextColor3 = Color3.fromRGB(unpack(tcolor))
-      end
-    else
-      if not btn_state then btn_state = true
-        new_btn.TextColor3 = Color3.fromRGB(0, 255, 0)
-      else btn_state = false
-        new_btn.TextColor3 = Color3.fromRGB(unpack(tcolor))
-      end
-    end if configs.set then update_configs() end
-  end) while btn_state or configs.buttons[varx] do wait(0.01)
-    script()
-  end return new_btn
-end
-
-main.removing_buttons = function()
-  for _, btn in pairs(screenui:GetChildren()) do
-    if btn:IsA("TextButton") then
-      btn:Destroy()
+function setup_folders()
+    if not (isfolder and makefolder and isfile and writefile and readfile) then return end
+    for _, f in ipairs(folders) do if not isfolder(f) then makefolder(f) end end
+    local file = folders[3] .. "/QuickButtons.json"
+    if not isfile(file) then
+        writefile(file, htp:JSONEncode({}))
     end
-  end do_notify("all buttons created has been removed!")
 end
 
-return main
--- First time ever make a UI_Library roblox --
+function load_configs()
+    local file = folders[3] .. "/QuickButtons.json"
+    if isfile and isfile(file) then
+        configs.buttons = htp:JSONDecode(readfile(file))
+    end
+end
+
+function save_configs()
+    if not configs.save then return end
+    writefile(folders[3] .. "/QuickButtons.json", htp:JSONEncode(configs.buttons))
+end
+
+function modules.set_configs(t)
+    if type(t) ~= "table" then return end
+    configs.save = t.saving_state or false
+    configs.ready = true
+    if configs.save then
+        setup_folders()
+        load_configs()
+    end
+end
+
+function modules.add_button(text, font, color, pos, corner, callback)
+    if not configs.ready then notify("use 'add_config({ saving_state = boolean })' first") return end
+    local b = Instance.new("TextButton", inst_s.screenui)
+    b.Name = randomstr(12)
+    b.Size = UDim2.fromScale(0.05, 0.1)
+    b.Position = UDim2.new(unpack(pos))
+    b.BackgroundColor3 = Color3.new(0, 0, 0)
+    b.BackgroundTransparency = 0.5
+    b.TextScaled = true
+    b.Text = text
+    b.TextColor3 = Color3.fromRGB(unpack(color))
+    b.Font = Enum.Font[font] or Enum.Font.Code
+    round_ui(b, corner)
+    b.MouseButton1Click:Connect(callback)
+    return b
+end
+
+function modules.add_toggle(text, font, color, pos, corner, callback, key)
+    if not configs.ready then notify("use 'add_config({ saving_state = boolean })' first") return end
+    if not key then notify("missing final argument 'string: toggle variable' name.") return end
+    local btn = modules.add_button(text, font, color, pos, corner, function() end)
+    local state = configs.buttons[key] == true
+    local is_running = false
+    local thread
+    local function update() btn.TextColor3 = state and Color3.fromRGB(0,255,0) or Color3.fromRGB(unpack(color)) end
+    local function start() if thread then return end is_running = true
+        thread = task.spawn(function()
+            while is_running do
+                task.wait()
+                callback()
+            end thread = nil
+        end)
+    end
+    local function plus_reload() update() if state then start() else is_running = false end end
+    btn.MouseButton1Click:Connect(function()
+        state = not state
+        configs.buttons[key] = state
+        save_configs()
+        plus_reload()
+    end) plus_reload()
+    return btn
+end
+
+function modules.removing_buttons()
+    for _, btn in ipairs(inst_s.screenui:GetChildren()) do
+        if btn:IsA("TextButton") then
+            print("Destroyed: " .. btn.Name .. "!")
+            btn:Destroy()
+        end
+    end notify("ALL UI-INSTANCE CLEARED!")
+end
+
+notify("Loaded UI-Modules by HoangHienXScripts!\n(First Time: v0.2)")
+return modules
